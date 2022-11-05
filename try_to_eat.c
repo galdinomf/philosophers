bf15587f1b25cb1fa070631e0f58169df8552c0a
Moacir Galdino <galdinomf@protonmail.com>
2022-11-05 12:52:34 -0300

    odd number of philosophers fixed

--- try_to_eat.c
+++ try_to_eat.c
@@ -6,7 +6,7 @@
 /*   By: mgaldino <mgaldino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 10:38:51 by mgaldino          #+#    #+#             */
-/*   Updated: 2022/11/03 15:17:55 by mgaldino         ###   ########.fr       */
+/*   Updated: 2022/11/05 12:48:53 by mgaldino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,17 +44,17 @@ void	unlock_fork_mutexes(int ind, int left_ind, t_data *data)
 
 int	take_fork(t_philo_ind *philo_ind, int fork_ind)
 {
-	int *forks;
+//	int *forks;
 	int	result;
 	t_data	*data;
 
 	result = 0;
 	data = philo_ind->data;
-	forks = philo_ind->data->forks;
+//	forks = philo_ind->data->forks;
 	pthread_mutex_lock(&data->forks_mutex[fork_ind]);
-	if (data->forks[fork_ind] == 0)
+	if ((data->forks[fork_ind] == 0) || (data->forks[fork_ind] == philo_ind->ind + 1))
 	{	
-		forks[fork_ind] = philo_ind->ind + 1;
+		//forks[fork_ind] = philo_ind->ind + 1;
 		display_message("has taken a fork", philo_ind);
 		result = 1;
 	}
@@ -62,7 +62,7 @@ int	take_fork(t_philo_ind *philo_ind, int fork_ind)
 	return (result);
 }
 
-void	lay_fork_down(t_philo_ind *philo_ind, int fork_ind)
+void	lay_fork_down(t_philo_ind *philo_ind, int fork_ind, int next_ind)
 {
 	int *forks;
 	t_data	*data;
@@ -71,7 +71,7 @@ void	lay_fork_down(t_philo_ind *philo_ind, int fork_ind)
 	forks = philo_ind->data->forks;
 	pthread_mutex_lock(&data->forks_mutex[fork_ind]);
 	if (forks[fork_ind] == philo_ind->ind + 1)
-		forks[fork_ind] = 0;
+		forks[fork_ind] = next_ind;
 	pthread_mutex_unlock(&data->forks_mutex[fork_ind]);
 }
 
@@ -97,6 +97,7 @@ int	try_to_eat(t_philo_ind *philo_ind)
 	t_data	*data;
 	int		ind;
 	int		left_ind;
+	int		right_ind;
 	int		has_not_eaten;
 
 	data = philo_ind->data;
@@ -106,6 +107,10 @@ int	try_to_eat(t_philo_ind *philo_ind)
 		left_ind = data->number_of_philosophers - 1;
 	else
 		left_ind = ind - 1;
+	if (ind == data->number_of_philosophers - 1)
+		right_ind =  0;
+	else
+		right_ind = ind + 1;
 	//printf("ind = %d, left_ind = %d\n", ind, left_ind);
 	//lock_fork_mutexes(ind, left_ind, data);
 	//printf("data->forks[%d] = %d\n", ind, data->forks[ind]);
@@ -123,8 +128,8 @@ int	try_to_eat(t_philo_ind *philo_ind)
 		has_not_eaten = 0;
 	//	lock_fork_mutexes(ind, left_ind, data);
 	}
-		lay_fork_down(philo_ind, ind);
-		lay_fork_down(philo_ind, left_ind);
+		lay_fork_down(philo_ind, ind, right_ind + 1);
+		lay_fork_down(philo_ind, left_ind, left_ind + 1);
 	//unlock_fork_mutexes(ind, left_ind, data);
 	/*
 	pthread_mutex_unlock(&data->forks_mutex[ind]);
